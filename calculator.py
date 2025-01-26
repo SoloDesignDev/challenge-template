a64ee5376f702fd0fededa6f80ad58a3592b2686
Solo Design <solodesigndevelopment@gmail.com>
2025-01-26 14:55:22 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+
+##### `calculator.py`
+```python
+# Calculator module
+
+def add(a, b):
+    """Addition: retourne la somme de a et b."""
+    # TODO: décommenter la ligne suivante pour valider la première étape
+    # return (a + b)
+
+def subtract(a, b):
+    """Soustraction: retourne la différence entre a et b."""
+    pass  # TODO: Implémentez cette fonction
+
+def multiply(a, b):
+    """Multiplication: retourne le produit de a et b."""
+    pass  # TODO: Implémentez cette fonction
+
+def divide(a, b):
+    """Division: retourne le quotient de a et b. Gère les divisions par zéro."""
+    pass  # TODO: Implémentez cette fonction
